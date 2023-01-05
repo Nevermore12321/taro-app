97b5f6708e369f5fb24ec2c9b18e9b6b45e5b561
Shaohe Guo <842125706@qq.com>
2023-01-05 14:27:43 +0800

    taro-app init

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,19 @@
+// eslint-disable-next-line no-undef
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/memo/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: 'NeverMore',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#7f8389',
+    selectedColor: '#5495e6',
+    borderStyle: 'black',
+    backgroundColor: '#fff',
   }
 })
